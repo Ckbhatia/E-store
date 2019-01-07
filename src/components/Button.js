06597ec20ccb04c52438237f86fcfe14dee90f2f
Ckbhatia <chetansain86@gmail.com>
2019-01-07 21:55:49 +0530

    feat: style button/cart by styled-components

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    text-transform: captilize;
+    font-size: 1.4rem;
+    background-color: transparent;
+    border: 0.05rem solid var(--LightGreen);
+    color: var(--LightGreen);
+    border-radius: 0.5rem;
+    padding: 0.2rem 0.4rem;
+    margin: 0.2rem 0.5rem 0.2rem 0;
+    cursor: pointer;
+    transition: all 0.5s ease-in-out;
+    &:hover {
+        color: var(--MainGreen);
+        background-color: var(--LightGreen);
+    }
+    &:focus {
+        outline: none;
+    }
+`
