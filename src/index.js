f1f7cb7b1fa8f991d8e6f69cc82f707b8c395226
Ckbhatia <chetansain86@gmail.com>
2019-01-14 15:30:34 +0530

    feat: wrapped rendering component to Context API

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { ProductProvider } from './Context';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, document.getElementById('root'));
+    <ProductProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ProductProvider>,
+     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
