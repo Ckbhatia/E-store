08c183e253fc78327490cf0f37c16728a75a9d4e
Ckbhatia <chetansain86@gmail.com>
2019-01-23 22:14:58 +0530

    feat: add details component

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,8 @@ export const ButtonContainer = styled.button`
     font-size: 1.4rem;
     background-color: transparent;
     border: 0.05rem solid var(--LightGreen);
-    color: var(--LightGreen);
+    border-color: ${props=> props.cart ? "var(--MainYellow)":"var(--lightGreen)"};
+    color: ${props=> props.cart ? "var(--MainYellow)": "var(--MainWhite)"};
     border-radius: 0.5rem;
     padding: 0.2rem 0.4rem;
     margin: 0.2rem 0.5rem 0.2rem 0;
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { ProductConsumer } from '../Context';
+import { Link } from 'react-router-dom';
+import { ButtonContainer } from './Button';
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h1>I'm details page</h1>
-            </div>
+            <ProductConsumer>
+                {value => {
+                    const {id, company, img, info, price, title, inCart} = 
+                    value.detailProduct;
+                    return (
+                        <div className="container py-5">
+                            <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                <h1>{title}</h1>
+                            </div>
+                        <div className="row">
+                            <div className="col-10 mx-auto.col-md-6 my-3">
+                                    <img src={img} alt="product" className="img-fluid"/>
+                            </div>
+                            <div className="col-10 mx-auto.col-md-6 my-3 text-capitlize">
+                                <h2>{title}</h2>
+                                <h4 className="text-title text-uppercase">
+                                {company}
+                                </h4>
+                                <h4 className="text-black">
+                                    <strong>price: <span>₹</span>{price}
+                                    </strong>
+                                </h4>
+                                <p className="text-muted lead">
+                                {info}
+                                </p>
+                                <div>
+                                    <Link to='/'>
+                                    <ButtonContainer cart>back to home</ButtonContainer>
+                                    </Link>
+                                    <ButtonContainer
+                                    cart
+                                    disabled={inCart?true:false}
+                                    onClick={() => {
+                                        value.addToCart(id);
+                                    }}>
+                                    {inCart? "inCart": "add to cart"}
+                                    </ButtonContainer>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    )
+                    }}
+            </ProductConsumer>
         );
     }
 }
