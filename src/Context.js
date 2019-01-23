fca176647a5932d63649c06027946df693bae056
Ckbhatia <chetansain86@gmail.com>
2019-01-23 22:11:16 +0530

    feat: add cart state, handleDetail and addToCart method

--- src/Context.js
+++ src/Context.js
@@ -6,7 +6,8 @@ const ProductContext = React.createContext();
 export class ProductProvider extends Component {
     state = {
         products: [],
-        detailProduct: detailProduct
+        detailProduct: detailProduct,
+        cart: []
     };
     componentDidMount() {
         this.setProducts();
@@ -16,15 +17,43 @@ export class ProductProvider extends Component {
         storeProducts.forEach(item => {
             const singleItem = {...item };
             tempProducts = [...tempProducts, singleItem]
-        })
+        });
         this.setState(() => {
             return { products: tempProducts}
-        })
-    }
+        });
+    };
+    getItem = id => {
+        const product = this.state.products.find(item => item.id === id);
+        return product;
+    };
+    
+    handleDetail = id => {
+        const product = this.getItem(id);
+        this.setState(()=> {
+            return {detailProduct: product};
+        });
+    };
+
+    addToCart = id => {
+        let tempProducts = [...this.state.products];
+        const index = tempProducts.indexOf(this.getItem(id));
+        const product = tempProducts[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
+        this.setState(()=> {
+            return {products: tempProducts, cart: [...this.state.cart,
+                product]};
+        }); 
+    };
+
     render() {
         return (
             <ProductContext.Provider value={{
-                ...this.state
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart
             }}>
                 {this.props.children}
             </ProductContext.Provider>
