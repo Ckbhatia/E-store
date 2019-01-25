ee1533b65b3161f40f0e682525c3997d647adfb1
Ckbhatia <chetansain86@gmail.com>
2019-01-25 22:06:28 +0530

    feat: create carts

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Title from '../Title';
+import CartColumns from './CartColumns';
+import EmptyCart from "./EmptyCart";
+import { ProductConsumer } from '../../Context';
+
+export default class Cart extends Component {
+    render() {
+        return (
+            <section>
+                <ProductConsumer>
+                    {value => {
+                        const { cart } = value;
+                        if(cart.lenght > 0) {
+                            return (
+                                <React.Fragment>
+                                    <Title name="My" title="cart" />
+                                    <CartColumns />
+                                </React.Fragment>
+                            );
+                        }
+                        else {
+                            return <EmptyCart />
+                        }
+                    }}
+                </ProductConsumer>
+            </section>
+        );
+    }
+}
