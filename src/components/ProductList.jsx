364f197f9cc190d371133dd5a02b66be572cfb7e
Ckbhatia <chetansain86@gmail.com>
2019-01-30 20:39:41 +0530

    fix: change the name and title in Title component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -10,7 +10,7 @@ export default class ProductList extends Component {
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
-                        <Title name="hi there" title="No" />
+                        <Title name="our" title="products" />
 
                         <div className="row">
                             <ProductConsumer>
