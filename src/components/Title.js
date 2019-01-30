7cddd606732aa7d07ffe6f41fd2c24a8c7e6da01
Ckbhatia <chetansain86@gmail.com>
2019-01-30 20:31:54 +0530

    fix: fix typos in class

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Title ({name, title}) {
     return (
         <div className="row">
-            <div className=".col-10.mx-auto.my-2.text-center.text-title">
-                <h1 className="text-capitalize font-weight-bold">
+            <div className="col-10 mx-auto my-2 text-center text-title">
+                <h1 className="text-capitalize">
                 {name} <strong className="">{title}</strong>
                 </h1>
             </div>
