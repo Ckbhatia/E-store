d4a9eef8502ea52fb67747e22b69440a484295c2
Ckbhatia <chetansain86@gmail.com>
2019-02-09 21:31:32 +0530

    fix: style default page

--- src/components/Default.jsx
+++ src/components/Default.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Default extends Component {
     render() {
         return (
-            <div>
-                <h1>The page not found 404</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-title-text-uppercase pt-5">
+                        <h1 className="display-3">404</h1>
+                        <h1>Error</h1>
+                        <h3>page not found</h3>
+                        <Link to='/' className="btn btn-black">Home</Link>
+                    </div>
+                </div>
             </div>
         );
     }
