c28aeb1c97f525d0b9537d175f922b2857faf22b
ckbhatia <chetansain86@gmail.com>
2020-03-30 14:19:44 +0530

    fix: change the nav link text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../logo.svg';
-import styled from 'styled-components';
-import {ButtonContainer} from './Button';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
-    render() {
-        return (
-                <Nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
-                    <Link to="/">
-                    <img src={Logo} alt='logo' className="navbar-brand" />
-                    </Link>
-                        <ul className="navbar-nav align-items-center">
-                            <li className="nav-item ml-5">
-                                <Link to="/" className="nav-link">
-                                    product
-                                </Link>
-                            </li>
-                        </ul>
-                        <Link to="/cart" className="ml-auto">
-                            <ButtonContainer>
-                                <span className="mr-2">
-                                    <i className="fas fa-cart-plus" />
-                                </span>
-                                My cart
-                            </ButtonContainer>
-                        </Link>
-                    </Nav>
-        )
-    }
+  render() {
+    return (
+      <Nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={Logo} alt="logo" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <span className="nav-main-text">BLT Store</span>
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            My cart
+          </ButtonContainer>
+        </Link>
+      </Nav>
+    );
+  }
 }
 
 const Nav = styled.nav`
-    background-color: var(--MainGreen);
-    .nav-link {
-        color: var(--MainWhite) !important;
-        font-size: 1.3rem;
-        text-transform: capitlize;
-    }
-`
+  background-color: var(--MainGreen);
+  .nav-link {
+    color: var(--MainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitlize;
+  }
+`;
