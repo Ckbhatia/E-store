d3ae5c9aeb01e7cdb98438b58fe5f09e5bb858e5
ckbhatia <chetansain86@gmail.com>
2020-03-31 23:47:04 +0530

    feat: add main products category data

---
+++ src/data/mainProducts.js
@@ -0,0 +1,16 @@
+export const mainProducts = [
+  {
+    id: 1,
+    title: "vegetables",
+    img: "img/mainCategory/vege.jpg",
+    info: "Fresh vegetables",
+    category: "vegetables"
+  },
+  {
+    id: 2,
+    title: "fruits",
+    img: "img/mainCategory/fruits.jpg",
+    info: "Fresh fruits",
+    category: "fruits"
+  }
+];
