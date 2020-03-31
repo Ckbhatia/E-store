2bd7648276b729647c15b655e21a24baac2ea547
ckbhatia <chetansain86@gmail.com>
2020-03-31 23:46:05 +0530

    feat: add new routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
-import ProductList from './components/ProductList';
-import Details from './components/Details';
-import Cart from './components/Cart/Cart';
-import Default from './components/Default';
-import {Switch, Route} from 'react-router-dom';
-import Modal from './components/Modal';
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart/Cart";
+import Default from "./components/Default";
+import { Switch, Route } from "react-router-dom";
+import Modal from "./components/Modal";
+import MainPage from "./components/MainPage";
+import { mainProducts } from "./data/mainProducts";
+import Checkout from "./components/checkout";
 
 class App extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Navbar />
-                <Switch>  
-                    <Route exact path="/" component={ProductList} />
-                    <Route path="/details" component={Details} />
-                    <Route path="/cart" component={Cart} />
-                    <Route component={Default} />
-                </Switch>
-                <Modal />
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/vegetables" component={ProductList} />
+          <Route path="/fruits" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/checkout" component={Checkout} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
