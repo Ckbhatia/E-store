0103741af5b00e51aac09250d369e11067c92281
ckbhatia <chetansain86@gmail.com>
2020-03-31 23:46:28 +0530

    feat: add banner data

---
+++ src/data/banner.js
@@ -0,0 +1,14 @@
+export const bannerData = [
+  {
+    id: 1,
+    title: "main banner",
+    img: "img/banner/organic.jpg",
+    info: "Fresh onions"
+  },
+  {
+    id: 2,
+    title: "immunity booster",
+    img: "img/banner/booster.jpg",
+    info: "immunity booster"
+  }
+];
