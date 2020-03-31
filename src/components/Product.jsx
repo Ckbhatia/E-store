a702c2b2bb95ed090602035f239b7f3d12490a52
ckbhatia <chetansain86@gmail.com>
2020-03-31 15:04:41 +0530

    fix: change the structure and layout

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,113 +1,119 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { ProductConsumer } from '../Context';
-import PropTypes from 'prop-types';
+import React, { Component, useContext } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductContext } from "../Context";
+import PropTypes from "prop-types";
 
-export default class Product extends Component {
-    render() {
-        const {id, title, img, price, inCart} = this.props.product;
-        return (
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-                <div className="card">
-                    <ProductConsumer>
-                        {(value) => (
-                            <div className="img-container p-5"
-                            onClick={() => value.handleDetail(id)}>
-                            <Link to='/details'>
-                                <img src={img} alt="product" className="card-img-top" />
-                            </Link>
-                            <button className="cart-btn" disabled={inCart ? true: false}
-                                onClick={() => {
-                                    value.addToCart(id);//Add product to cart
-                                    value.openModal(id);//Open modal when product get in cart
-                                }}
-                                >
-                                
-                                {inCart ? (
-                                <p className="text-capitalize mb-0" disabled>
-                                    {" "} In cart
-                                </p> ) : (
-                                <i className="fas fa-cart-plus" />
-                                )}
-                            </button>
-                        </div>
-                        )}
-                    </ProductConsumer>
-                    <div className="card-footer d-flex justify-content-between">
-                    <p className="algin-self-center mb0">
-                    {title}</p>
-                     <h5 className="text-blue font-italic mb-0">
-                     <span className="mr-1">₹</span>
-                     {price}
-                     </h5>
-                    </div>
-                </div>
-            </ProductWrapper>
-        );
-    }
+export default function Product({ product }) {
+  const { handleDetail, addToCart, openModal } = useContext(ProductContext);
+  const { id, title, img, price, inCart, quantity } = product;
+
+  return (
+    <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+      <div className="card">
+        <div className="img-container p-5" onClick={() => handleDetail(id)}>
+          <Link to="/details">
+            <img src={img} alt="product" className="card-img-top" />
+          </Link>
+        </div>
+        <div className="card-footer">
+          <div className="container-one mr-2">
+            <p className="algin-self-center mb0">{title}</p>
+          </div>
+          <div className="container-two d-flex justify-content-between">
+            <p className="algin-self-center mb0">{quantity}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">₹</span>
+              {price}
+            </h5>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                addToCart(id); //Add product to cart
+                // value.openModal(id);//Open modal when product get in cart
+              }}
+            >
+              {inCart ? (
+                <Link
+                  to="cart"
+                  className="text-capitalize mb-0"
+                  title="Go to cart"
+                >
+                  <button className="to-cart-btn">In Cart</button>
+                </Link>
+              ) : (
+                <span className="cart-btn-container" title="add to cart">
+                  <i className="fas fa-cart-plus" />
+                </span>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+    </ProductWrapper>
+  );
 }
 
 Product.propType = {
-    product:PropTypes.shape({
-        id:PropTypes.number,
-        img:PropTypes.string,
-        title:PropTypes.string,
-        price:PropTypes.number,
-        inCart:PropTypes.bool
-    }).isRequired
-}
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool
+  }).isRequired
+};
 
 const ProductWrapper = styled.div`
-    .cart{
-        border-color: transparent;
-        transition: all 1s linear;
+  &:hover {
+    .card {
+      box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
     }
-    .cart-footer{
-        background: transparent;
-        border-top: transparent;
-        transition: all 1s linear;
-    }
+  }
+
+  .card-footer {
+    // min-height: 100px;
+  }
+
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .card-img-top {
+    transition: all 0.3s linear;
+    // width: 8rem;
+    // height: 6rem;
+    width: 100%;
+  }
+
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+
+  .cart-btn {
+    background-color: transparent;
+    border: none;
+    color: var(--MainGreen);
+    font-size: 1.4rem;
+  }
+
+  .to-cart-btn {
+    font-size: 1rem;
+    color: #25a641;
+    background: transparent;
+    border: 1px solid #25a641;
+    border-radius: 2px;
     &:hover {
-        .card {
-            border: 0.04rem solid rgba(0,0,0,0.2);
-            box-shadow: 2px 2px 2px rgba(0,0,0,0.2);
-        }
-        .card-footer {
-            background-color: rgba(247, 247, 247);
-        }
+      background-color: #25a641;
+      color: #fff;
     }
-    .img-container {
-        position: relative;
-        overflow: hidden;
-    }
-    .card-img-top {
-        transition: all 0.3s linear;
-        width: 8rem;
-        height: 6rem;
-    }
-    .img-container:hover .card-img-top {
-        transform: scale(1.2);
-    }
-    .cart-btn {
-        position: absolute;
-        bottom: 0;
-        right: 0;
-        padding: 0.2rem 0.4rem;
-        background-color: var(--LightGreen);
-        border: none;
-        color: var(--MainWhite);
-        font-size: 1.4rem;
-        border-radius: 0.5rem 0 0 0;
-        transform: translate(100%, 100%);
-        transition: all 0.3s linear;
-    }
-    .img-container:hover .cart-btn {
-        transform: translate(0, 0);
-    }
-    .cart-btn:hover {
-        color: var(--MainWhite);
-        background-color: var(--MainGreen);
-    }
-    
-    `;
+  }
+  .img-container:hover .cart-btn {
+    transform: translate(0, 0);
+  }
+  .cart-btn:hover {
+    color: var(--LightGreen);
+  }
+`;
