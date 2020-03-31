d6709826784f280bfa9f4eda2c9765cf128a972c
ckbhatia <chetansain86@gmail.com>
2020-03-31 23:48:04 +0530

    feat: create dummy products and data

---
+++ src/data/products.js
@@ -0,0 +1,181 @@
+const products = {
+  vegetables: [
+    {
+      id: 1,
+      title: "Onion - प्याज )",
+      quantity: "1 kg",
+      img: "img/vegetables/onion.jpg",
+      price: 33,
+      company: "BLT STORE",
+      iinfo: "The fresh handpicked onions, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      title: "Potato - आलू ",
+      quantity: "1 kg",
+      img: "img/vegetables/potato.jpg",
+      price: 36,
+      company: "BLT STORE",
+      info: "The handpicked potatoes, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      title: "Ginger - अदरक",
+      quantity: "100 g",
+      img: "img/vegetables/ginger.jpg",
+      price: 12,
+      company: "BLT STORE",
+      info: "The handpicked potatoes, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 4,
+      title: "Onion - प्याज )",
+      quantity: "1 kg",
+      img: "img/vegetables/onion.jpg",
+      price: 33,
+      company: "BLT STORE",
+      iinfo: "The fresh handpicked onions, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 5,
+      title: "Potato - आलू ",
+      quantity: "1 kg",
+      img: "img/vegetables/potato.jpg",
+      price: 36,
+      company: "BLT STORE",
+      info: "The handpicked potatoes, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 6,
+      title: "Ginger - अदरक",
+      quantity: "100 g",
+      img: "img/vegetables/ginger.jpg",
+      price: 12,
+      company: "BLT STORE",
+      info: "The handpicked potatoes, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 7,
+      title: "Onion - प्याज )",
+      quantity: "1 kg",
+      img: "img/vegetables/onion.jpg",
+      price: 33,
+      company: "BLT STORE",
+      iinfo: "The fresh handpicked onions, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 8,
+      title: "Potato - आलू ",
+      quantity: "1 kg",
+      img: "img/vegetables/potato.jpg",
+      price: 36,
+      company: "BLT STORE",
+      info: "The handpicked potatoes, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 9,
+      title: "Ginger - अदरक",
+      quantity: "100 g",
+      img: "img/vegetables/ginger.jpg",
+      price: 12,
+      company: "BLT STORE",
+      info: "The handpicked potatoes, price might change day by day.",
+      inCart: false,
+      count: 0,
+      total: 0
+    }
+  ],
+
+  fruits: [
+    {
+      id: 1,
+      title: "Fresh Onion ( प्याज )",
+      quantity: "1 kg",
+      img: "img/onion.jpg",
+      price: 33,
+      company: "BLT STORE",
+      info: "Fresh onions",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      title: "Oreo Chocolatey",
+      quantity: "50 g",
+      img: "img/product02.jpeg",
+      price: 9,
+      company: "Oreo",
+      info:
+        "The vanila flavoured buiscuit, known for the test. It's too tasty and healty. Ingredients: Refined Wheat Flour, Sugar, Edible Vegetable Fat, Palmolein Oil, Invert Syrup, Cocoa Solids, Leavening Agents, Edible Salt",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      title: "Unibic Assorted Cookies",
+      quantity: "450 g, Pack of 6",
+      img: "img/product03.jpeg",
+      price: 8,
+      company: "Unibic",
+      info:
+        "The super save pack of tastie cookies. Ingredients: Wheat Flour, Sugar, Edible Vegetable OIl, Milk Solids, Liquid Solids, Edible Common Salt, Leavening Agents, Emulsifiers",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 4,
+      title: "Britannia Premium Rusk",
+      quantity: "273 g",
+      img: "img/product04.jpeg",
+      price: 38,
+      company: "Britannia",
+      info:
+        "The favourite of indian family toasts. Taste to believe, Ingredients: Refined Wheat Flour, Sugar, Edible Oil, Yeast, Invert Sugar Syrup, Milk Solids, Butter, Edible Common Salt, Spices,",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 5,
+      title: "Parle Monaco Biscuits",
+      quantity: "200 g",
+      img: "img/product05.jpeg",
+      price: 29,
+      company: "Parle",
+      info:
+        "The old is gold. Parle best one ever buiscuit. Ingredients: Wheat Flour, Edible Vegetable Oils (Palm Oil, Palmolein Oil), Sugar, Raising Agents, Invert Sugar Syrup, Edible Common Salt, Yeast, Acidity Regulators, Emulsifiers, Dough Conditioner and Improver Amylase & Other Enzymes",
+      inCart: false,
+      count: 0,
+      total: 0
+    }
+  ]
+};
+
+export default products;
