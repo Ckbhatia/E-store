e20e148c4cbecdad4cac8a87fce2074df95f559e
ckbhatia <chetansain86@gmail.com>
2020-04-05 18:03:59 +0530

    feat: add state to retain data

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
@@ -7,9 +7,15 @@ import { MdEmail, MdCall } from "react-icons/md";
 import support from "../data/support";
 
 export default function OrderModal(props) {
-  const cart = props.cartdetails[0];
+  const [productsSize, updateProductsSize] = useState(null);
+  const [totalPrice, updateTotalPrice] = useState(null);
+
   const orderId = props.cartdetails[4];
-  const cartTotal = props.cartdetails[3];
+
+  useEffect(() => {
+    updateProductsSize(props.cartdetails[0].length);
+    updateTotalPrice(props.cartdetails[3]);
+  }, []);
 
   return (
     <>
@@ -28,13 +34,14 @@ export default function OrderModal(props) {
           <h4>Order ID: {orderId}</h4>
           <h4>
             Total payable amount for{" "}
-            {cart && cart.length === 1
-              ? `${cart && cart.length} item:`
-              : `${cart && cart.length} items:`}{" "}
-            ₹{cartTotal}
+            {productsSize === 1
+              ? `${productsSize} item:`
+              : `${productsSize} items:`}{" "}
+            ₹{totalPrice}
           </h4>
           <p>Payment Mode: UPI ( DIGITAL )</p>
-          <p>Estimated delivery: next day morning</p>
+          <p>Delivery Type: Standard</p>
+          <p>Estimated delivery: next day morning ( 9:00 AM to 12:00 PM)</p>
           <span>Support : </span>
           <span>
             <MdCall /> {support.contact.phone}{" "}
