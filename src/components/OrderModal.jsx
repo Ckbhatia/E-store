fac3189ff6f47415a0ad9df84ef21e8707f00e17
ckbhatia <chetansain86@gmail.com>
2020-04-13 18:12:00 +0530

    feat: add orderType and condition

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -11,6 +11,7 @@ export default function OrderModal(props) {
   const [totalPrice, updateTotalPrice] = useState(null);
 
   const orderId = props.cartdetails[4];
+  const orderType = props.cartdetails[5];
 
   useEffect(() => {
     updateProductsSize(props.cartdetails[0].length);
@@ -32,13 +33,19 @@ export default function OrderModal(props) {
         </Modal.Header>
         <Modal.Body>
           <h4>Order ID: {orderId}</h4>
-          <h4>
-            Total payable amount for{" "}
-            {productsSize === 1
-              ? `${productsSize} item:`
-              : `${productsSize} items:`}{" "}
-            ₹{totalPrice}
-          </h4>
+          {orderType === "grocery" ? (
+            <h4>
+              We will notify you about the estimated price of your grocery order
+            </h4>
+          ) : (
+            <h4>
+              Total payable amount for{" "}
+              {productsSize === 1
+                ? `${productsSize} item:`
+                : `${productsSize} items:`}{" "}
+              ₹{totalPrice}
+            </h4>
+          )}
           <p>Payment Mode: UPI ( DIGITAL )</p>
           <p>Delivery Type: Standard</p>
           <p>Estimated delivery: next day morning ( 9:00 AM to 12:00 PM)</p>
