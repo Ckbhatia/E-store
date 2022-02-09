959c685b924db74d9ab66287ff859b56a68930ec
Ckbhatia <chetansain86@gmail.com>
2022-02-09 11:52:30 +0530

    feat: add helper functions

--- src/services/product.js
+++ src/services/product.js
@@ -1,5 +1,13 @@
 import config from "../config";
 
-export const getProducts = (tags) => {
-  return fetch(`${config.baseUrl}/api/v1/product/tags=${tags}`);
+export const getProductsByCategory = (category) => {
+  return fetch(`${config.baseUrl}/api/v1/product?category=${category}`);
+}
+
+export const getProductsByTags = (tags) => {
+  return fetch(`${config.baseUrl}/api/v1/product?tags=${tags}`);
+}
+
+export const getProductById = (id) => {
+  return fetch(`${config.baseUrl}/api/v1/product/${id}`);
 }
