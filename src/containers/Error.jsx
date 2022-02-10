5712b6060cc4743a02bd66d4370b093d5e23c69e
Ckbhatia <chetansain86@gmail.com>
2022-02-10 22:18:39 +0530

    feat: create Error component

---
+++ src/containers/Error.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+
+const Error = () => {
+  const handleClick = () => {
+    if (window) {
+      window.location.reload();
+    }
+  };
+
+  return (
+    <StyledMainContainer>
+      <div>
+        <h1>Something went wrong</h1>
+        <div>
+          <button onClick={handleClick} className="btn">
+            Reload
+          </button>
+        </div>
+      </div>
+    </StyledMainContainer>
+  );
+};
+
+export default Error;
+
+const StyledMainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  
+  margin-top: 200px;
+  
+  h1 {
+    font-size: 2rem;
+    color: var(--MainGreen);
+  }
+  
+  > div {
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  button {
+    font-size: 16px;
+    margin-top: 20px;
+    border-radius: 5px;
+    border: 1px solid var(--MainGreen);
+    color: var(--MainGreen);
+    width: 142px;
+  }
+
+  @media (max-width: 768px) {
+    button {
+      width: 100px;
+      font-size: 14px;
+    }
+
+    h1 {
+      font-size: 1.8rem;
+    }
+  }
+`;
